bf9c2ddd5c3f6c1de6f09ee1ee7f3463ce5c431a
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-04-14 15:19:49 +0200

    Added Functions to ShortAnswer

--- SupportTools/SupportToolsUi/ShortAnswer.xaml.cs
+++ SupportTools/SupportToolsUi/ShortAnswer.xaml.cs
@@ -2,8 +2,19 @@
 
 public partial class ShortAnswer : ContentPage
 {
+	ListView listView = new ListView();
+	
 	public ShortAnswer()
 	{
 		InitializeComponent();
+	}
+
+	protected override void OnBindingContextChanged()
+	{
+		base.OnBindingContextChanged();
+
+		listView.SetBinding(ItemsView.ItemsSourceProperty, "ShortAnswers");
+		// Add some function to fill list;
+		
 	}
 }
