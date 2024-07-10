fe6b14dee6993a2d37d890656a643aa301c92bd0
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-07-10 11:44:43 +0200

    added TestPage

--- SupportTools/SupportToolsUi/ShortAnswer.xaml.cs
+++ SupportTools/SupportToolsUi/ShortAnswer.xaml.cs
@@ -4,46 +4,12 @@ using Microsoft.Maui.Layouts;
 
 namespace SupportTools;
 
-public class TestClass
-{
-	public string Name{get; set;}
-	public string Text2{get; set;}
-}
-
-public class ManagerClass
-{
-	private int count = 0;
-	public ObservableCollection<TestClass> lolz{get;} = new ObservableCollection<TestClass>();
-
-	public void addAnswers()
-	{
-		string zahl = count.ToString();
-		lolz.Add(new TestClass{Name = "Antwort", Text2 = zahl.ToString()});
-		Console.WriteLine(lolz.Count.ToString());
-		count++;
-	}
-
-	public string TestString{get;} = "Data out of Managerclass";
-}
-
 public partial class ShortAnswer : ContentPage
 {
-	//public ObservableCollection<Answer> shortAnswers = new ObservableCollection<Answer>();
 
 	public ShortAnswer()
 	{
 		InitializeComponent();
-		ManagerClass mc = new();
-	
-		mc.addAnswers();
-
-		TestClass tc = new()
-		{
-			Name = "Backend-Text",
-			Text2 = "Dies ist der 2. Backend-Text"
-		};
-
-		BindingContext = new {mc, tc};
-		//BindingContext = tc;
+		
 	}
 }
--- SupportTools/SupportToolsUi/TestPage.xaml.cs
+++ SupportTools/SupportToolsUi/TestPage.xaml.cs
@@ -1,11 +1,58 @@
 using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Layouts;
 
 namespace SupportTools;
 
+public class TestClass
+{
+	public string Name{get; set;}
+	public string Text2{get; set;}
+}
+
+public class ManagerClass
+{
+	private int count = 0;
+	public ObservableCollection<TestClass> lolz{get;} = new ObservableCollection<TestClass>();
+
+	public void addAnswers()
+	{
+		string zahl = count.ToString();
+		lolz.Add(new TestClass{Name = "Antwort", Text2 = zahl.ToString()});
+		Console.WriteLine(lolz.Count.ToString());
+		count++;
+	}
+
+	public string TestString{get;set;} = "Data out of Managerclass";
+}
+
+public class ViewModel
+{
+	public ManagerClass mc1{get;} = new ManagerClass();
+}
+
 public partial class TestPage : ContentPage
 {
-    public TestPage()
-    {
-        InitializeComponent();
-    }
+	//public ObservableCollection<Answer> shortAnswers = new ObservableCollection<Answer>();
+
+	public TestPage()
+	{
+		InitializeComponent();
+		
+		ManagerClass mc = new();
+		ViewModel model = new();
+
+		model.mc1.addAnswers();
+		model.mc1.addAnswers();
+
+		TestClass tc = new()
+		{
+			Name = "Backend-Text",
+			Text2 = "Dies ist der 2. Backend-Text"
+		};
+
+		BindingContext = new {tc, model, mc};
+		//BindingContext = new {mc, tc, model};
+		//BindingContext = tc;
+	}
 }
